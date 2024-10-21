9c0400c2dbfe614510357493f65533b58a4a93f8
Navidreza80 <navidrezaabbaszadeh80@gmail.com>
2024-10-21 22:59:31 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/index.css",
+    "./src/components/login/login.tsx",
+    "./src/components/forgetPassword/forgetPassword.tsx"
+  ],
+  theme: {
+    fontFamily: {
+      'DannaThin': ['DannaThin'],
+      'DannaExtraLight': ['DannaExtraLight'],
+      'DannaLight': ['DannaLight'],
+      'DannaNormal': ['DannaNormal'],
+      'DannaDemiBold': ['DannaDemiBold'],
+      'DannaBold': ['DannaBold'],
+      'DannaExtraBold': ['DannaExtraBold'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
